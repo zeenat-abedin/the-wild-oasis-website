5fa9425f7d092f682a39ae309a3034ea76436579
zeenat <zeenu.23@gmail.com>
2024-06-29 23:02:26 +0530

    update RootLayout function to accept children
    and add main element to wrap children

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,13 @@
 import Navigation from "./components/Navigation";
 
-export default function RootLayout() {
+export default function RootLayout({children}) {
   return (
     <html lang="en">
       <body>
-        <Navigation/>
+        <Navigation />
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   )
