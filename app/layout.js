6c155501addd5eaaf04008eee067518af2020b1a
zeenat <zeenu.23@gmail.com>
2024-06-29 23:15:07 +0530

    add metadata export
    and update RootLayout function in layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,9 @@
 import Navigation from "./components/Navigation";
 
+export const metadata = {
+  title: "The Wild Oasis",
+}
+
 export default function RootLayout({children}) {
   return (
     <html lang="en">
