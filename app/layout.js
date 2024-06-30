abdac7d7c085d4b4a6d45c962c721bc1161598a0
zeenat <zeenu.23@gmail.com>
2024-06-30 16:57:13 +0530

    add blue bg

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({children}) {
   return (
     <html lang="en">      
-      <body>
+      <body className="bg-blue-900">
         <header>
         <Logo/>
         <Navigation />
