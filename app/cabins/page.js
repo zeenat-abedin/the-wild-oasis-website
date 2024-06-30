f39035bc0d39dc33302d857b124923ebbecddde0
zeenat <zeenu.23@gmail.com>
2024-06-30 15:13:19 +0530

    update Logo component to use Next.js Link
    removed Counter component and API call from Cabins page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,19 +1,8 @@
-import Counter from "@/app/_components/Counter"
 
-export default async function Page() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users')
-  const data = await res.json()
-  console.log(data)
+export default  function Page() {
   return (
     <div>
       <h1>Cabins Page</h1>
-      <ul>
-        {data.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
-
-      <Counter users={data} />
     </div>
   )
 }
