22a54a87bf221a8a7134825f42d38ee489034449
zeenat <zeenu.23@gmail.com>
2024-06-30 10:10:47 +0530

    add async keyword to Page function and fetch API call

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,15 @@
-export default function Page(){
-  return <h1>Cabins Page</h1>
+export default async function Page() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
+  console.log(data)
+  return (
+    <div>
+      <h1>Cabins Page</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
